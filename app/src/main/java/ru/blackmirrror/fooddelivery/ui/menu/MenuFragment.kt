5bdb54276a39261511db1f29a87cb8d5dc44598e
Ekaterina Petrova <ekaterinapetrova985@gmail.com>
2024-03-26 16:38:44 +0300

    Part of UI

---
+++ app/src/main/java/ru/blackmirrror/fooddelivery/ui/menu/MenuFragment.kt
@@ -0,0 +1,37 @@
+package ru.blackmirrror.fooddelivery.ui.menu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import ru.blackmirrror.fooddelivery.databinding.FragmentMenuBinding
+
+class MenuFragment : Fragment() {
+
+    private var _binding: FragmentMenuBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val menuViewModel =
+            ViewModelProvider(this).get(MenuViewModel::class.java)
+
+        _binding = FragmentMenuBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
